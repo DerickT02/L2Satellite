c5ff5b349bfe1b110828008200d2566d11d5e628
DerickT02 <derickt18@outlook.com>
2023-04-24 17:58:26 -0700

    Sun in center of screen

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import turtle
+from sun import Sun
+
+if __name__ == "__main__":
+    screen = turtle.Screen()
+    screen.setup(1400, 900)
+    screen.bgcolor("black")
+    screen.tracer(0)
+
+    sun = Sun("yellow", (0, 0))
+
+    while True:
+        screen.update()
+
